b570e692c71215ce254734de53480c9b0ffde455
CreazyHans <creazyhans@gmail.com>
2025-11-17 11:24:07 -0400

    Update __app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
 // /pages/_app.js
 
 import '../styles/globals.css';
+import { Inter } from 'next/font/google';
+
+// Configura la fuente con los subsets que necesitas
+const inter = Inter({ subsets: ['latin'] });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    // Aplica la clase de la fuente al componente principal
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
 export default MyApp;
